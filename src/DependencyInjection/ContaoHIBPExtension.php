5dfbd6751ff7ef9fb41dc707f85adf6bf208e7b9
Stefan Meise <stefan.meise@gmx.net>
2020-04-27 13:59:57 +0200

    Composer.json update

--- src/DependencyInjection/ContaoHIBPExtension.php
+++ src/DependencyInjection/ContaoHIBPExtension.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace con2net\ContaoHelloWorldBundle\DependencyInjection;
+namespace con2net\ContaoHIBPBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
